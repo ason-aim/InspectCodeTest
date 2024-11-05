db15157e88d7a34aea2fc00360e2dbfa08f76b5f
ason <ason@aiming-inc.com>
2024-11-05 12:11:54 +0900

    プロジェクト初期化

---
+++ client/Assets/Test/TestBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviourScript : MonoBehaviour
+{
+    private int aa = 0;
+    private int bb = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogWarning(aa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
