562f182f736a12118ffd09a6f9e0b5f1da7ab3d6
ason <ason@aiming-inc.com>
2024-11-07 19:29:33 +0900

    簡潔な出力が出来た

--- tools/Inspectcode/inspectcode.sh
+++ tools/Inspectcode/inspectcode.sh
@@ -11,15 +11,18 @@ else
     dotnet tool install -g JetBrains.ReSharper.GlobalTools --arch arm64
 fi
 
-# 分析、複数のcsprojを一緒に実行したい
+# 分析
 # output分析結果出力先は、実行ファイルと同じフォルダ
+# TODO : severityは、実際のfanプロジェクトで検証してから決める
 jb inspectcode ./client/client.sln \
   --build \
   --eXtensions=JetBrains.Unity \
   --include="**/TestBehaviourScript.cs;**/TestScript.cs" \
   --settings="./client/client.sln.DotSettings" \
-  --output=./tools/Inspectcode/inspectcode.json
+  --severity=INFO \
+  --format=Text \
+  --output=./tools/Inspectcode/inspectcode.txt
 
-# ログ確認用、本番は要らないと思う
+# TODO : ログ確認用、本番は要らないと思う
 echo "Press Enter to exit..."
 read
