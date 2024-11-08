454d70050b6d7370af8f783d330b66c3df01ce86
ason <ason@aiming-inc.com>
2024-11-08 13:56:20 +0900

    外部からファイルPath指定出来るように

--- tools/Inspectcode/inspectcode.sh
+++ tools/Inspectcode/inspectcode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# ドキュメント：https://github.com/Aiminginc2/fan/issues/7769
+
 # fanフォルダに移動
 cd ../..
 
@@ -11,13 +13,21 @@ else
     dotnet tool install -g JetBrains.ReSharper.GlobalTools --arch arm64
 fi
 
+# 外部からの解析するファイルを指定する引数、このまま実行するとデフォルトのAssets下全ての.csが解析対象になる
+include_files=${1:-"Assets/**.cs"}
+# 外部指定の場合は\nで分割してください。例："Assets/Test/TestBehaviourScript.cs\nAssets/Test/TestScript.cs\n"
+include_files=$(printf "%b" "$include_files" | tr '\n' ';')
+
 # 分析
-# output分析結果出力先は、実行ファイルと同じフォルダ
+# settingsはRiderのプロジェクト設定と同じものを参照している
 # TODO : severityは、実際のfanプロジェクトで検証してから決める
+# output分析結果出力先は、実行ファイルと同じフォルダ
+# format=Textが一番簡潔なフォーマット、他は[Sarif, Html, Xml]など細かい出力が設定出来る
 jb inspectcode ./client/client.sln \
   --build \
+  --swea \
   --eXtensions=JetBrains.Unity \
-  --include="Assets/Test/TestBehaviourScript.cs;Assets/Test/TestScript.cs" \
+  --include="$include_files" \
   --settings="./client/client.sln.DotSettings" \
   --severity=INFO \
   --format=Text \
